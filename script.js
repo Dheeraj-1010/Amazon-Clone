6fbce02c9103fc8c5f8c1893e5ccbe569d68d905
Dheeraj yadav <dheerajyadavgunawal@gmail.com>
2024-10-11 11:29:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Simulate an API request or any async operation
+    setTimeout(() => {
+        hideLoader();
+        showContent();
+    }, 1500); // Replace with your actual data loading logic and time
+
+    function hideLoader() {
+        const loader = document.getElementById("loader");
+        loader.style.display = "none";
+    }
+
+    function showContent() {
+        const content = document.getElementById("content");
+        content.style.display = "block";
+    }
+});
